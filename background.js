c04f2860a6164b3bd59f0e9530757c017f0f78e7
looterliam <liamtimmerman9@gmail.com>
2024-06-18 16:20:01 -0400

    Update background.js

--- background.js
+++ background.js
@@ -5,40 +5,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         console.log("The user canceled the request.");
         return;
       }
-      navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-          mandatory: {
-            chromeMediaSource: 'desktop',
-            chromeMediaSourceId: streamId
-          }
-        }
-      }).then((stream) => {
-        const video = document.createElement('video');
-        video.srcObject = stream;
-        video.onloadedmetadata = () => {
-          video.play();
-          const canvas = document.createElement('canvas');
-          canvas.width = video.videoWidth;
-          canvas.height = video.videoHeight;
-          const context = canvas.getContext('2d');
-          context.drawImage(video, 0, 0, canvas.width, canvas.height);
-          const imgUrl = canvas.toDataURL();
-          downloadImage(imgUrl);
-          stream.getTracks()[0].stop();
-        };
-      }).catch((err) => {
-        console.error("Error: " + err);
-      });
+
+      // Send a message to the content script to capture the screen with the provided streamId
+      chrome.tabs.sendMessage(sender.tab.id, { action: 'startCapture', streamId: streamId, area: request.area });
     });
   }
 });
-
-function downloadImage(dataUrl) {
-  const a = document.createElement('a');
-  a.href = dataUrl;
-  a.download = 'screenshot.png';
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-}
